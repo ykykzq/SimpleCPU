1ddb8e7d6ba23ccf8ac002e97826696902a159b6
hang liu <635880182@qq.com>
2024-08-23 13:52:17 +0800

    fix:修正ALU对sll的处理

--- test/myCPU/alu.v
+++ test/myCPU/alu.v
@@ -77,7 +77,7 @@ assign xor_result = alu_src1 ^ alu_src2;
 assign lui_result = alu_src2;
 
 // SLL result
-assign sll_result = alu_src2 << alu_src1[4:0];   //rj << i5
+assign sll_result = alu_src1 << alu_src2[4:0];   //rj << i5
 
 // SRL, SRA result
 assign sr64_result = {{32{op_sra & alu_src2[31]}}, alu_src2[31:0]} >> alu_src1[4:0]; //rj >> i5
