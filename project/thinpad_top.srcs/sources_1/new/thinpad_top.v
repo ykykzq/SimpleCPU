4861b9ec6ffed5ce387eb67370a2a6d28d57e470
hang liu <635880182@qq.com>
2024-08-31 15:22:34 +0800

    try:尝试修timing loop

--- project/thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ project/thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -77,16 +77,113 @@ always@(posedge clk_10M or negedge locked) begin
     else        reset_of_clk10M <= 1'b0;
 end
 
-always@(posedge clk_10M or posedge reset_of_clk10M) begin
-    if(reset_of_clk10M)begin
+/*always@(posedge clk_55M or posedge reset_of_clk10M) begin
+    if(reset_of_clk60M)begin
         // Your Code
     end
     else begin
         // Your Code
     end
+end*/
+
+// 不使用内存、串口时，禁用其使能信号
+//assign base_ram_ce_n = 1'b1;
+//assign base_ram_oe_n = 1'b1;
+//assign base_ram_we_n = 1'b1;
+
+//assign ext_ram_ce_n = 1'b1;
+//assign ext_ram_oe_n = 1'b1;
+//assign ext_ram_we_n = 1'b1;
+
+// 数码管连接关系示意图，dpy1同理
+// p=dpy0[0] // ---a---
+// c=dpy0[1] // |     |
+// d=dpy0[2] // f     b
+// e=dpy0[3] // |     |
+// b=dpy0[4] // ---g---
+// a=dpy0[5] // |     |
+// f=dpy0[6] // e     c
+// g=dpy0[7] // |     |
+//           // ---d---  p
+
+// 7段数码管译码器演示，将number用16进制显示在数码管上面
+wire[7:0] number;
+SEG7_LUT segL(.oSEG1(dpy0), .iDIG(number[3:0])); //dpy0是低位数码管
+SEG7_LUT segH(.oSEG1(dpy1), .iDIG(number[7:4])); //dpy1是高位数码管
+
+reg[15:0] led_bits;
+assign leds = led_bits;
+
+always@(posedge clock_btn or posedge reset_btn) begin
+    if(reset_btn)begin //复位按下，设置LED为初始值
+        led_bits <= 16'h1;
+    end
+    else begin //每次按下时钟按钮，LED循环左移
+        led_bits <= {led_bits[14:0],led_bits[15]};
+    end
 end
 
+/* //直连串口接收发送演示，从直连串口收到的数据再发送出去
+wire [7:0] ext_uart_rx;
+reg  [7:0] ext_uart_buffer, ext_uart_tx;
+wire ext_uart_ready, ext_uart_clear, ext_uart_busy;
+reg ext_uart_start, ext_uart_avai;
+    
+assign number = ext_uart_buffer;
 
+async_receiver #(.ClkFrequency(50000000),.Baud(9600)) //接收模块，9600无检验位
+    ext_uart_r(
+        .clk(clk_50M),                       //外部时钟信号
+        .RxD(rxd),                           //外部串行信号输入
+        .RxD_data_ready(ext_uart_ready),  //数据接收到标志
+        .RxD_clear(ext_uart_clear),       //清除接收标志
+        .RxD_data(ext_uart_rx)             //接收到的一字节数据
+    );
+
+assign ext_uart_clear = ext_uart_ready; //收到数据的同时，清除标志，因为数据已取到ext_uart_buffer中
+always @(posedge clk_50M) begin //接收到缓冲区ext_uart_buffer
+    if(ext_uart_ready)begin
+        ext_uart_buffer <= ext_uart_rx;
+        ext_uart_avai <= 1;
+    end else if(!ext_uart_busy && ext_uart_avai)begin 
+        ext_uart_avai <= 0;
+    end
+end
+always @(posedge clk_50M) begin //将缓冲区ext_uart_buffer发送出去
+    if(!ext_uart_busy && ext_uart_avai)begin 
+        ext_uart_tx <= ext_uart_buffer;
+        ext_uart_start <= 1;
+    end else begin 
+        ext_uart_start <= 0;
+    end
+end
+
+async_transmitter #(.ClkFrequency(50000000),.Baud(9600)) //发送模块，9600无检验位
+    ext_uart_t(
+        .clk(clk_50M),                  //外部时钟信号
+        .TxD(txd),                      //串行信号输出
+        .TxD_busy(ext_uart_busy),       //发送器忙状态指示
+        .TxD_start(ext_uart_start),    //开始发送信号
+        .TxD_data(ext_uart_tx)        //待发送的数据
+    );
+
+//图像输出演示，分辨率800x600@75Hz，像素时钟为50MHz
+wire [11:0] hdata;
+assign video_red = hdata < 266 ? 3'b111 : 0; //红色竖条
+assign video_green = hdata < 532 && hdata >= 266 ? 3'b111 : 0; //绿色竖条
+assign video_blue = hdata >= 532 ? 2'b11 : 0; //蓝色竖条
+assign video_clk = clk_50M;
+vga #(12, 800, 856, 976, 1040, 600, 637, 643, 666, 1, 1) vga800x600at75 (
+    .clk(clk_50M), 
+    .hdata(hdata), //横坐标
+    .vdata(),      //纵坐标
+    .hsync(video_hsync),
+    .vsync(video_vsync),
+    .data_enable(video_de)
+); */
+/* =========== Demo code end =========== */
+
+    
 
 myCPU_top myCPU_top(
     .clk                (clk_10M        ),
@@ -114,109 +211,5 @@ myCPU_top myCPU_top(
 );
 
 
-
-
-
-
-
-///////////////////////////////////////////////////////////////
-/// 下面是模板演示内容，在我们的设计中直接注释
-
-// // 不使用内存、串口时，禁用其使能信号
-// assign base_ram_ce_n = 1'b1;
-// assign base_ram_oe_n = 1'b1;
-// assign base_ram_we_n = 1'b1;
-
-// assign ext_ram_ce_n = 1'b1;
-// assign ext_ram_oe_n = 1'b1;
-// assign ext_ram_we_n = 1'b1;
-
-// // 数码管连接关系示意图，dpy1同理
-// // p=dpy0[0] // ---a---
-// // c=dpy0[1] // |     |
-// // d=dpy0[2] // f     b
-// // e=dpy0[3] // |     |
-// // b=dpy0[4] // ---g---
-// // a=dpy0[5] // |     |
-// // f=dpy0[6] // e     c
-// // g=dpy0[7] // |     |
-// //           // ---d---  p
-
-// // 7段数码管译码器演示，将number用16进制显示在数码管上面
-// wire[7:0] number;
-// SEG7_LUT segL(.oSEG1(dpy0), .iDIG(number[3:0])); //dpy0是低位数码管
-// SEG7_LUT segH(.oSEG1(dpy1), .iDIG(number[7:4])); //dpy1是高位数码管
-
-// reg[15:0] led_bits;
-// assign leds = led_bits;
-
-// always@(posedge clock_btn or posedge reset_btn) begin
-//     if(reset_btn)begin //复位按下，设置LED为初始值
-//         led_bits <= 16'h1;
-//     end
-//     else begin //每次按下时钟按钮，LED循环左移
-//         led_bits <= {led_bits[14:0],led_bits[15]};
-//     end
-// end
-
-// //直连串口接收发送演示，从直连串口收到的数据再发送出去
-// wire [7:0] ext_uart_rx;
-// reg  [7:0] ext_uart_buffer, ext_uart_tx;
-// wire ext_uart_ready, ext_uart_clear, ext_uart_busy;
-// reg ext_uart_start, ext_uart_avai;
-    
-// assign number = ext_uart_buffer;
-
-// async_receiver #(.ClkFrequency(50000000),.Baud(9600)) //接收模块，9600无检验位
-//     ext_uart_r(
-//         .clk(clk_50M),                       //外部时钟信号
-//         .RxD(rxd),                           //外部串行信号输入
-//         .RxD_data_ready(ext_uart_ready),  //数据接收到标志
-//         .RxD_clear(ext_uart_clear),       //清除接收标志
-//         .RxD_data(ext_uart_rx)             //接收到的一字节数据
-//     );
-
-// assign ext_uart_clear = ext_uart_ready; //收到数据的同时，清除标志，因为数据已取到ext_uart_buffer中
-// always @(posedge clk_50M) begin //接收到缓冲区ext_uart_buffer
-//     if(ext_uart_ready)begin
-//         ext_uart_buffer <= ext_uart_rx;
-//         ext_uart_avai <= 1;
-//     end else if(!ext_uart_busy && ext_uart_avai)begin 
-//         ext_uart_avai <= 0;
-//     end
-// end
-// always @(posedge clk_50M) begin //将缓冲区ext_uart_buffer发送出去
-//     if(!ext_uart_busy && ext_uart_avai)begin 
-//         ext_uart_tx <= ext_uart_buffer;
-//         ext_uart_start <= 1;
-//     end else begin 
-//         ext_uart_start <= 0;
-//     end
-// end
-
-// async_transmitter #(.ClkFrequency(50000000),.Baud(9600)) //发送模块，9600无检验位
-//     ext_uart_t(
-//         .clk(clk_50M),                  //外部时钟信号
-//         .TxD(txd),                      //串行信号输出
-//         .TxD_busy(ext_uart_busy),       //发送器忙状态指示
-//         .TxD_start(ext_uart_start),    //开始发送信号
-//         .TxD_data(ext_uart_tx)        //待发送的数据
-//     );
-
-// //图像输出演示，分辨率800x600@75Hz，像素时钟为50MHz
-// wire [11:0] hdata;
-// assign video_red = hdata < 266 ? 3'b111 : 0; //红色竖条
-// assign video_green = hdata < 532 && hdata >= 266 ? 3'b111 : 0; //绿色竖条
-// assign video_blue = hdata >= 532 ? 2'b11 : 0; //蓝色竖条
-// assign video_clk = clk_50M;
-// vga #(12, 800, 856, 976, 1040, 600, 637, 643, 666, 1, 1) vga800x600at75 (
-//     .clk(clk_50M), 
-//     .hdata(hdata), //横坐标
-//     .vdata(),      //纵坐标
-//     .hsync(video_hsync),
-//     .vsync(video_vsync),
-//     .data_enable(video_de)
-// );
-/* =========== Demo code end =========== */
 
 endmodule
